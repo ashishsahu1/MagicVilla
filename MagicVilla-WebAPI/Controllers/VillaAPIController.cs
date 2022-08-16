b8a51e8d476b716c3d6760e223a5d803eb97f78a
Ashish Sahu <ashishsahu10428@gmail.com>
2022-08-16 21:08:12 +0530

    Create first EndPoint - Use [Route] attribute in controller class - use [HttpVerb] attribute in endpoint method - return the data

--- MagicVilla-WebAPI/Controllers/VillaAPIController.cs
+++ MagicVilla-WebAPI/Controllers/VillaAPIController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_WebAPI.Controllers
 {
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-
+        [HttpGet]
+        public IEnumerable<Villa> getVilla()
+        {
+            List<Villa> villa = new List<Villa>
+            {
+                new Villa{Id=1,Name="Pool View"},
+                new Villa{Id=2,Name="Beach View"},
+                new Villa{Id=3,Name="City View"}
+            };
+            return villa;
+        }
     }
 }
