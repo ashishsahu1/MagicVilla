4ed07c757ea4fb854f4ad6dee67e8088d2418635
Ashish Sahu <ashishsahu10428@gmail.com>
2022-08-17 12:41:41 +0530

    DTO Added - Wrapper to model object

--- MagicVilla-WebAPI/Controllers/VillaAPIController.cs
+++ MagicVilla-WebAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,4 @@
-﻿using MagicVilla_WebAPI.Models;
+﻿using MagicVilla_WebAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_WebAPI.Controllers
@@ -8,13 +8,14 @@ namespace MagicVilla_WebAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> getVilla()
+        public IEnumerable<VillaDTO> getVilla()
         {
-            List<Villa> villa = new List<Villa>
+            List<VillaDTO> villa = new List<VillaDTO>
             {
-                new Villa{Id=1,Name="Pool View"},
-                new Villa{Id=2,Name="Beach View"},
-                new Villa{Id=3,Name="City View"}
+                new VillaDTO{Id=1,Name="Pool View"},
+                new VillaDTO{Id=2,Name="Beach View"},
+                new VillaDTO{Id=3,Name="City View"},
+                new VillaDTO{Id=3,Name="Night View"}
             };
             return villa;
         }
